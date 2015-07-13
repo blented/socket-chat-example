ca8be3cf857c97c57b6d94276556778440b5fc16
Grant Miller <blented@gmail.com>
2015-07-13 09:41:20 -0700

    fixes, docs, added python listener

--- index.js
+++ index.js
@@ -1,34 +1,74 @@
 var app = require('express')()
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
+var os = require('os')
+var config = require('./config.js')
+var _ = require('lodash')
+
+
+var port = config.port
 
 app.get('/', function(req, res)
 {
-  res.sendFile('index.html', {root: __dirname})
+	res.sendFile('index.html', {root: __dirname})
 })
 
 io.on('connection', function(socket)
 {
-  console.log('a user connected')
-  socket.broadcast.emit('hi')
+	console.log('a user connected')
+	socket.broadcast.emit('hi')
 
-  socket.on('disconnect', function()
-  {
-    console.log('user disconnected')
-  })
+	socket.on('disconnect', function()
+	{
+		console.log('user disconnected')
+	})
 
-  socket.on('chat message', function(msg)
-  {
-    console.log('message: ' + msg)
-  })
+	socket.on('chat message', function(msg)
+	{
+		console.log('message: ' + msg)
+	})
 
-  socket.on('chat message', function(msg)
-  {
-    io.emit('chat message', msg)
-  })
+	socket.on('chat message', function(msg)
+	{
+		io.emit('chat message', msg)
+	})
 })
 
-http.listen(3000, function()
+http.listen(port, function()
 {
-  console.log('listening on *:3000')
+	console.log('\n\nServer listening at:\n')
+
+	// loop through the networkInterfaces and list
+	// each IPv4 that we find
+	var prefix
+	var IPs = ''
+	_.each(os.networkInterfaces(), function(i)
+	{
+		_.each(i, function(entry)
+		{
+			if (entry.family == 'IPv4')
+			{
+				if (entry.internal)
+				{
+					prefix = '(internal)'
+				}
+				else if (entry.address.slice(0, 3) == '10.' ||
+					entry.address.slice(0, 4) == '172.' ||
+					entry.address.slice(0, 4) == '192.')
+				{
+					prefix = '(private network)'
+				}
+				else
+					prefix = '(public)'
+
+				console.log(prefix + ' http://' +
+					entry.address + ':' +
+					port)
+
+				if (!entry.internal)
+					IPs += '<li>' + entry.address + '</li>\n'
+			}
+		})
+	})
+	IPs = IPs.slice(0, -1)
 })
---
+++ readme.md
@@ -0,0 +1,24 @@
+## Simple socket.io chat example
+
+Simple chat to show how sockets work.  Runs on node, listener for Python as well.
+
+## Installation
+```
+npm install
+pip install socketIO-client
+```
+
+## Usage
+Command window one:
+```
+node index.js
+```
+
+Command window two:
+```
+python listener.py
+```
+
+Web browser: [http://127.0.0.1:3000](http://127.0.0.1:3000)
+
+Type chats, see them displayed :)
